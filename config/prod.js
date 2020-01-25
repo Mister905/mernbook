a931a284049a4f99676517ecc8633809d07f5df8
James McCarthy <jamesmccarthyxci@gmail.com>
2020-01-24 20:01:27 -0500

    Trying something

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 // prod.js - unlike dev.js we commit this to git!!!
 module.exports = {
-  mongo_uri: process.env.MONGO_URI
+  mongo_uri: process.env.MONGO_URI,
+  jwt_secret: process.env.JWT_SECRET,
 };
